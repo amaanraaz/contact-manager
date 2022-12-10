fe3b22a382279ced239b71202bd57504695bfc3c
amaanraaj <amandwivedi00@gmail.com>
2022-12-11 01:27:13 +0530

    Updatin the delete functionality

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import Header from './components/Header';
 import AddContact from './components/AddContact';
 import ContactList from './components/ContactList';
 import { useState } from 'react';
+import { v4 as uuid } from "uuid";
+
 
 function App() {
   const [contacts,setContact] = useState([]);
+
+  // adding new contact
   const addContactHandle = (name,email,phone)=>{
-    setContact([...contacts,{name,email,phone}]);
+    setContact([...contacts,{id:uuid(),name,email,phone}]);
+  }
+
+  // removing the contact
+  const removeContactHandle = (id)=>{
+    const newList = contacts.filter((contact)=>{
+      return contact.id !== id;
+    });
+    setContact(newList);
   }
   return (
     <div className="App">
       <Header />
-      <AddContact addContactHandle = {addContactHandle}/>
-      <ContactList contacts={contacts} />
+      <AddContact addContactHandle = {addContactHandle} />
+      <ContactList contacts={contacts} removeContactHandle={removeContactHandle} />
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 const ContactList = (props)=>{
+    const removeContact = (id)=>{
+        props.removeContactHandle(id);
+    }
+
     const cont_list = props.contacts.map((contact)=>{
         return (
-            <div>
+            <div key={contact.id}>
                 <div>{contact.name}</div>
                 <div>{contact.email}</div>
                 <div>{contact.phone}</div>
-                <div><button>Delete</button></div>
+                {/* passing arrow fn as in on click arguments cant be passed */}
+                <div><button onClick={()=>removeContact(contact.id)}>Delete</button></div>
             </div>
         )
     })
